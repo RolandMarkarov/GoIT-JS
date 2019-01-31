6fcf72d08ac3394bd96ea77219e10c6d14824eaa
RolandMarkarov <markarovr@gmail.com>
2019-01-31 14:01:07 +0200

    HW6

--- HW5/index.js
+++ HW5/index.js
@@ -138,7 +138,7 @@ console.log(getAllUniqueSkills(users));
 /**
 * Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
 */
-const getUserNamesSortedByFriendsCount = arr => {return arr.sort((a, b)=> a.friends.length - b.friends.length).map(el=> el.name)};
+const getUserNamesSortedByFriendsCount = arr => {return arr.sort((a, b)=> a.friends.length - b.friends.length).map(el=> el.name )};
 
 console.log(getUserNamesSortedByFriendsCount(users)); 
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
